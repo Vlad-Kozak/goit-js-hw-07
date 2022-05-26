90932eb0a0884a86dde318f4c65cad68e8019c83
Vlad-Kozak <k911k12@gmail.com>
2022-05-26 21:59:44 +0300

    add render

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const items = galleryItems
+   .map(({ original, preview, description }) => {
+      return `
+   <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+         />
+      </a>
+   </div>`;
+   })
+   .join("");
+
+galleryEl.innerHTML = items;
